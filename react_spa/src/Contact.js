5e717de35dd0a1a13f83e6f8ae70b6e7a987edf2
Mike Igoe <mikeigoe@gmail.com>
2018-02-13 22:46:00 +0000

    Initial commit

---
+++ react_spa/src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Looking for more information?</h2>
+        <p>Click here to start a live chat with a 
+        a <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">customer support agent</a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
---
+++ react_spa/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./index.css";
+
+// Tutorial https://www.kirupa.com/react/creating_single_page_app_react_using_react_router.htm
+
+ReactDOM.render(
+    <Main/>,
+    document.getElementById("root")
+);
+
+
